1bd88bf6caa3d0ebea5cb5337413b86a467bf1e1
Harmen <harmenstoppels@gmail.com>
2016-12-06 09:22:16 +0100

    stop earlier

--- 95/95.cc
+++ 95/95.cc
@@ -2,6 +2,8 @@
 #include <vector>
 #include <map>
 
+using it = std::vector<size_t>::const_iterator;
+
 std::vector<size_t> primes_below(size_t N)
 {
   std::vector<bool> prime(N, true);
@@ -22,52 +24,33 @@ std::vector<size_t> primes_below(size_t N)
   return primes;
 }
 
-void construct(
-  std::vector<size_t>::const_iterator current,
-  std::vector<size_t>::const_iterator last,
-  std::map<size_t, size_t> &factors,
-  size_t product,
-  size_t sum,
-  size_t const N
-)
+size_t construct(it current, it last, size_t product, size_t sum, size_t const N)
 {
-  size_t prime_factor = *current;
 
-  // No factors left or the denominator gets too high.
-  if (current == last || product > N)
+  // Sum of proper divisors is too large
+  if (sum > N + product)
+    return 1;
+
+  // Last prime factor or too large number.
+  if (current == last || product * (*current) > N)
+    return 0;
+
+  size_t total = 0;
+
+  // Multiply by prime factor as long as product <= N
+  for (size_t sum_factor = 1, multiplier = 1; product <= N; product *= *current)
   {
-    std::cout << product << " = ";
-    for (const auto &factor : factors)
-      for (size_t idx = 0; idx < factor.second; ++idx)
-        std::cout << factor.first << ' ';
-
-    std::cout << '(' << sum << ")\n";
-    return;
-  }
-
-  // Skip this prime number
-  construct(current + 1, last, factors, product, sum, N);
-
-  // Or include this prime number 1 or more times.
-  factors[prime_factor] = 1;
-  size_t sum_factor = prime_factor + 1;
-  size_t multiplier = prime_factor;
-
-  for (product *= prime_factor; product <= N; product *= prime_factor)
-  {
-    construct(current + 1, last, factors, product, sum_factor * sum, N);
-    ++factors[prime_factor];
-    multiplier *= prime_factor;
+    total += construct(current + 1, last, product, sum_factor * sum, N);
+    multiplier *= *current;
     sum_factor += multiplier;
   }
-  factors.erase(prime_factor);
+
+  return total;
 }
 
 int main()
 {
-  size_t const N = 100'000;
+  size_t const N = 1'000'000;
   auto primes = primes_below(N);
-
-  std::map<size_t, size_t> guys;
-  construct(primes.begin(), primes.end(), guys, 1, 1, N);
+  std::cout << construct(primes.begin(), primes.end(), 1, 1, N) << '\n';
 }
