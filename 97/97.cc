45c4b78a25ea74e3d586aa0f30b6d56cfc25d212
Harmen <harmenstoppels@gmail.com>
2016-12-08 15:39:07 +0100

    97

--- 97/97.cc
+++ 97/97.cc
@@ -3,65 +3,93 @@
 #include <algorithm>
 #include <iterator>
 
-using ten_digits = std::array<uint16_t, 10>;
-
-ten_digits to_ten_digits(size_t num)
+template <class T, size_t N>
+class FixedDigits
 {
-  ten_digits ten;
-  ten.fill(0);
-  for (size_t idx = 0; num != 0; ++idx)
+  template<class U, size_t M>
+  friend std::ostream &operator<<(std::ostream &os, FixedDigits<U, M> rhs);
+  
+  std::array<T, N> digits;
+
+public:
+  FixedDigits()
   {
-    ten[idx] = num % 10;
-    num /= 10;
+    digits.fill(0);
   }
-  return ten;
-}
 
-ten_digits prod_10_digits(ten_digits const &lhs, ten_digits const &rhs)
-{
-  ten_digits product;
-  product.fill(0);
-
-  for (size_t idx = 0; idx < 10; ++idx)
-    for (size_t l = 0; l <= idx; ++l)
-      product[idx] += lhs[l] * rhs[idx - l];
-
-  for (size_t idx = 0; idx < 10; ++idx)
+  FixedDigits(size_t num)
   {
-    if (product[idx] > 10)
+    digits.fill(0);
+    for (size_t idx = 0; num != 0 && idx < N; ++idx)
     {
-      uint16_t carry = product[idx] / 10;
-      product[idx] %= 10;
-
-      if (idx + 1 < 10)
-        product[idx + 1] += carry;
+      digits[idx] = num % 10;
+      num /= 10;
     }
   }
 
-  return product;
+  FixedDigits &operator*=(FixedDigits const &rhs)
+  {
+    return *this = (*this) * rhs;
+  }
+
+  FixedDigits operator*(FixedDigits const &rhs) const
+  {
+    FixedDigits product;
+
+    // Product
+    for (size_t idx = 0; idx < N; ++idx)
+      for (size_t l = 0; l <= idx; ++l)
+        product.digits[idx] += digits[l] * rhs.digits[idx - l];
+
+    // Overflow
+    for (size_t idx = 0; idx < N; ++idx)
+    {
+      if (product.digits[idx] > 10)
+      {
+        T carry = product.digits[idx] / 10;
+        product.digits[idx] %= 10;
+
+        if (idx + 1 < N)
+          product.digits[idx + 1] += carry;
+      }
+    }
+
+    return product;
+  }
+};
+
+template<class T, size_t N>
+std::ostream &operator<<(std::ostream &os, FixedDigits<T, N> rhs)
+{
+  std::copy(rhs.digits.rbegin(), rhs.digits.rend(), std::ostream_iterator<size_t>(os));
+  return os;
 }
 
-ten_digits mod_10_to_the_10(ten_digits prod, ten_digits base, size_t power)
+template<class T, size_t N>
+FixedDigits<T, N> mod_N(FixedDigits<T, N> prod, FixedDigits<T, N> base, size_t power)
 {
   while (power != 1)
   {
     if (power % 2 != 0)
     {
-      prod = prod_10_digits(prod, base);
+      prod *= base;
       --power;
       continue;
     }
 
-    base = prod_10_digits(base, base);
+    base *= base;
     power /= 2;
   }
 
-  return prod_10_digits(prod, base);
+  return prod * base;
 }
 
 int main()
 {
-  auto digits = mod_10_to_the_10(to_ten_digits(28433), to_ten_digits(2), 7830457);
-  std::copy(digits.rbegin(), digits.rend(), std::ostream_iterator<size_t>(std::cout));
-  std::cout << '\n';
+  FixedDigits<size_t, 10000> prod(28433);
+  FixedDigits<size_t, 10000> base(2);
+
+  auto digits = mod_N(prod, base, 7830457);
+  
+  std::cout << digits << '\n';
 }
