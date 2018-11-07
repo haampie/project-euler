66abeae27fed013d7dae3be14c8fc2e9a0b35665
Harmen Stoppels <harmenstoppels@gmail.com>
2018-11-07 13:12:47 +0100

    113

--- 113/113.cc
+++ 113/113.cc
@@ -1,60 +1,28 @@
 #include <iostream>
-#include <set>
 #include <algorithm>
-#include <iterator>
+#include <array>
+#include <numeric>
 
-size_t decr(size_t depth = 4, size_t digit = 9)
+size_t count(size_t length)
 {
-    size_t total = 1;
+    size_t sum = 0;
+    std::array<size_t, 10> f;
+    std::fill(f.begin(), f.end(), 1);
 
-    if (depth != 0)
-        for (int i = digit; i >= 0; --i)
-            total += decr(depth - 1, i);
-
-    return total;
-}
-
-size_t incr(size_t depth = 4, size_t digit = 1)
-{
-    size_t total = 1;
-
-    if (depth != 0)
-        for (size_t i = digit; i < 10; ++i)
-            total += incr(depth - 1, i);
-
-    return total;
-}
-
-bool is_bouncy(size_t n)
-{    
-    bool up = false;
-    bool down = false;
-    
-    size_t prevd = n % 10;
-    n /= 10;
-
-    while (n)
+    for (size_t i = 0; i < length; ++i)
     {
-        size_t next_d = n % 10;
+        for (auto d : f)
+            sum += 2 * d;
 
-        if (next_d > prevd) up = true;
-        else if (next_d < prevd) down = true;
-        if (up && down) return true;
+        sum -= f[9];
 
-        prevd = next_d;
-        n /= 10;
+        std::partial_sum(f.begin(), f.end(), f.begin());
     }
 
-    return false;
+    return sum - 10 * length;
 }
 
 int main()
 {
-    incr(5);
-    // for (size_t len = 1; len < 30; ++len)
-    // {
-    //     size_t multiple = 10 * len + 2;
-    //     size_t total = incr(len) + decr(len) - multiple;
-    //     std::cout << total << '\n';
-    // }
+    std::cout << count(100) << '\n';
 }
